890779a6582e84646f85e2acbf45b1d4d68a6778
Ayush Gupta <ayushdgupta17@gmail.com>
2023-03-15 13:16:18 +0530

    add @timeout fault-tolerance property in our API and the API we are calling from it is taking 20 sec so fallback methid will be called after 1 + 3 retries

--- src/main/java/com/guptaji/resource/IndiaTvSeriesData.java
+++ src/main/java/com/guptaji/resource/IndiaTvSeriesData.java
@@ -3,6 +3,7 @@ package com.guptaji.resource;
 import com.guptaji.proxyInterface.TvSeriesApiProxy;
 import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.jboss.logmanager.Logger;
 
@@ -26,14 +27,25 @@ public class IndiaTvSeriesData {
     // 1 normal execution + 3 Retries but if for all retries as well we will not get any output then in the
     // last i.e.after 3 retries fallback method will be hit.
 
+    // in @timeout we've provided 1000 ms i.e. 1 sec wait time for our API so if the response will not come
+    // in the given time then Retry will be there for 3 times then fallback.
+
     @GET
     @Fallback(fallbackMethod = "getTvSeriesCountByCountryFallbackMethod")
     @Retry(maxRetries = 3)
+    @Timeout(1000)
     @Path("/{countryName}")
     @Produces(MediaType.TEXT_PLAIN)
     public Response getTvSeriesCountByCountry(@PathParam("countryName") String countryName){
         LOGGER.info("TvSeriesDataByCountry API is called");
-        return tvSeriesApiProxy.getTvSeriesCountByCountry(countryName);
+        
+        Long startTime = System.currentTimeMillis();
+        Response response = tvSeriesApiProxy.getTvSeriesCountByCountry(countryName);
+        Long endTime = System.currentTimeMillis();
+
+        String resp = response.readEntity(String.class);
+
+        return Response.ok(resp + " Time taken by API is "+ (endTime - startTime)/ 1000).build();
     }
 
     public Response getTvSeriesCountByCountryFallbackMethod(String countryName){
